8d61c7714ccc670cd90c8602112d2533fb867b8b
mohand abdalbari <mohand.mostafa9811@gmail.com>
2021-03-06 16:59:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+let big_title = document.getElementById('big-title')
+big_title.innerHTML = 'hi my name is mohand and i love the leader'
+big_title.style = 'background-color:red;padding:40px 0;color:white;text-align:center;font-family:arial;'
