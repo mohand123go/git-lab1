6c30362abf0eaad082df613591026379830b3614
mohand abdalbari <mohand.mostafa9811@gmail.com>
2021-03-07 20:19:54 +0200

    el gamzaaaa

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let big_title = document.getElementById('big-title')
 big_title.innerHTML = 'hi my name is mohand and i love the leader'
 big_title.style = 'background-color:red;padding:40px 0;color:white;text-align:center;font-family:arial;'
+let El_gmaza = 'ana harooo el algmaza'
