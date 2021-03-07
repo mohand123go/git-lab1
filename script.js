7c31ad89b231e15b89ead30e2cdb977627007620
Abdelrahman <abdelrahman.amr.997@gmail.com>
2021-03-07 20:19:09 +0200

    update 1 from Abdelrahman

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let big_title = document.getElementById('big-title')
 big_title.innerHTML = 'hi my name is mohand and i love the leader'
 big_title.style = 'background-color:red;padding:40px 0;color:white;text-align:center;font-family:arial;'
+
+
+console.log(`hello ya maaaannn saba7o`);
